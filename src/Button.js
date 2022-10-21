73f2f9abada51443eb11feabde57901a2174deda
junghwan-kim <hairmetal@naver.com>
2022-10-21 14:34:39 +0900

    First React Movie App Practice

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import propTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Button({text, onClick}){
+    return (
+        <button className={styles.btn} onClick={onClick}>{text}</button>
+    )
+}
+
+Button.propTypes = {
+    text: propTypes.string.isRequired
+};
+
+export default Button;
